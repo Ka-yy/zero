0fd74da411f5b806c591336e0f50ee5de72a1d22
Kayy <145061317+Ka-yy@users.noreply.github.com>
2025-01-08 15:02:37 +0100

    update main
    kept my pythondoc

--- main.py
+++ main.py
@@ -1,4 +1,29 @@
-import argparse
+# import argparse   
+ """ 
+ TODO: make a terminal help for the usage of the app 
+     the usage of the app should look like:  
+     '''shell 
+         ## Input  
+         zero --help 
+         # OUTPUT  
+         Usage: select send or receive to send/recieve files  
+         arguments 
+         --send, -s [send] 
+                     [Protocols] 
+                     -ftp  
+                     -sftp 
+                     -http 
+                     -htttps  
+                         --ip:port/dns:port [location of receiver] 
+                             -- path/to/file(s) 
+         --receive, -r [receive] 
+     ''' 
+ TODO: use argparse to give a choice, select whether to send or receive (client/server) 
+  
+ NOTE: to use the predefinded functions, create a new object and then call the methods 
+ """ 
+
+ import argparse
 from operations.client import client
 from operations.server import Server
 import sys
